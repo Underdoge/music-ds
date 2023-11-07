2a75925c09ff9ee751dd1a09b22c040458fcbcd7
Eduardo Chapa <eduardo.chapa@gmail.com>
2023-11-07 16:14:02 -0600

    Cleanning up the code

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+"""
+This is a streamlit app that will showcase an hypothetical use case for the
+model created under 'models/music_size.joblib'.
+"""
 import streamlit as st
 import polars as pl
 from joblib import load
 from util.on_change import update_slider, update_numin
 from util.scaler import scale
+from util.data_imputation import data_imputation
 import plotly.express as px
 import numpy as np
 
@@ -25,85 +30,13 @@ They provided us their current music files, and we were able to create a model\
 """
 )
 
-songs_df = pl.read_csv('data/music_library_export.csv')
-songs_size = songs_df.select(pl.col('Size'))
-songs_independent_vars = songs_df.select((pl.col('Total Time')/1000),
-                                         pl.col('Bit Rate'))
+# Import dataset
+
+original_df = pl.read_csv('data/music_library_export.csv')
 
 # Data imputation
 
-genres = songs_df.select(
-    pl.col("Genre").map_elements(
-        lambda x: "Alternative" if x == "Alternativo" else x)
-    .map_elements(
-        lambda x: "Electronic" if x == "Electronica" else x)
-    .map_elements(
-        lambda x: "Electronic Pop" if x in ["Pop Electronica",
-                                            "Electronica / Pop"] else x)
-    .map_elements(
-        lambda x: "Indie" if x == "indie" else x)
-    .map_elements(
-        lambda x: "Indie Rock" if x in ["Rock/Indie", "Indie/Rock",
-                                        "General Indie Rock"] else x)
-    .map_elements(
-        lambda x: "Miscellaneous" if x == "misc" else x)
-    .map_elements(
-        lambda x: "Soundtrack" if x in ["soundtrack", "Banda sonora"] else x)
-    .map_elements(
-        lambda x: "Thrash Metal" if x == "Thrash Metal" else x)
-    .map_elements(
-        lambda x: "Alt Rock" if x in ["Alt. Rock", "Alternative Rock",
-                                      "Rock alternativo",
-                                      "Alternative, Rock",
-                                      "General Alternative Rock"] else x)
-    .map_elements(
-        lambda x: "Brit Pop" if x == "Brit-pop" else x)
-    .map_elements(
-        lambda x: "Pop Rock" if x in ["Pop/Rock", "Pop/Rock 2000's"] else x)
-    .map_elements(
-        lambda x: "Pop" if x == "General Pop" else x)
-    .map_elements(
-        lambda x: "Folk" if x == "General Folk" else x)
-    .map_elements(
-        lambda x: "Rock" if x in ["General Rock", "Rock En General",
-                                  "Rock en general", "Rock @",
-                                  "rock"] else x)
-    .map_elements(
-        lambda x: "Heavy Metal" if x == "Rock Duro Y Heavy" else x)
-    .map_elements(
-        lambda x: "Hip Hop/Rap" if x == "General Rap/Hip-Hop" else x)
-    .map_elements(
-        lambda x: "Bitpop" if x == "bitpop" else x)
-    .map_elements(
-        lambda x: "Chillstep" if x == "chillstep" else x)
-    .map_elements(
-        lambda x: "Chiptune" if x == "chiptune" else x)
-    .map_elements(
-        lambda x: None
-        if x in ["genre", "default", ".", "(255)", "Other"] else x)
-    .map_elements(
-        lambda x: "Unclassifiable" if x == "General Unclassifiable" else x)
-    .map_elements(
-        lambda x: "Soft Rock / Alternative Folk / Folk / Rock"
-        if x == "soft rock/alternative folk/folk/rock" else x)
-    .alias("Genre")
-).to_series()
-
-songs_df = songs_df.with_columns(genres.alias("Genre"))
-songs_df.select(
-    pl.col('Genre')
-).unique().sort("Genre")
-
-songs_df = songs_df.drop_nulls(["Genre"])
-songs_df = songs_df.drop_nulls(["Year"])
-songs_df = songs_df.filter(pl.col('Year') > 1000).filter(pl.col('Year') < 2024)
-songs_df.drop_in_place("Comments")
-songs_df.drop_in_place("File Folder Count")
-songs_df.drop_in_place("Library Folder Count")
-songs_df.drop_in_place("Kind")
-songs_df.drop_in_place("Location")
-
-# End of Data Imputation
+songs_df = data_imputation(original_df)
 
 st.markdown(
     """
@@ -215,17 +148,23 @@ predicted_size = model.predict(scaled_new_df)
 st.write("Total predicted storage size (GBs): ",
          round(predicted_size.sum()/(1024*1024*1024), 2))
 
-# Fix negative sizes
+# Add results to new_df
 
-size_series = pl.Series("Size", np.concatenate(predicted_size)).map_elements(
-    lambda x: -x if x < 0 else x
-)
-
-# Plot results
+size_series = pl.Series("Size", np.concatenate(predicted_size))
 
 new_df = new_df.with_columns(
     size_series
 ).rename({"Total Time": "Duration"})
+
+# Remove negative sizes
+
+new_df = new_df.select(
+    pl.col('Duration').filter(pl.col('Size') > 0),
+    pl.col('Bit Rate').filter(pl.col('Size') > 0),
+    pl.col('Size').filter(pl.col('Size') > 0)
+)
+
+# Plot results
 
 fig_2 = px.scatter_3d(
     new_df,
---
+++ util/data_imputation.py
@@ -0,0 +1,88 @@
+"""
+This module contains the 'data_imputation' function that cleans up the
+provided dataframe.
+"""
+import polars as pl
+from polars import DataFrame
+
+
+def data_imputation(songs_df: DataFrame) -> DataFrame:
+    """This function will clean up the provided DataFrame and return it.
+
+    Args:
+        songs_df (DataFrame): A dataframe with the songs dataset.
+
+    Returns:
+        DataFrame: The cleaned up polars DataFrame.
+    """
+    genres = songs_df.select(
+        pl.col("Genre").map_elements(
+            lambda x: "Alternative" if x == "Alternativo" else x)
+        .map_elements(
+            lambda x: "Electronic" if x == "Electronica" else x)
+        .map_elements(
+            lambda x: "Electronic Pop" if x in ["Pop Electronica",
+                                                "Electronica / Pop"] else x)
+        .map_elements(
+            lambda x: "Indie" if x == "indie" else x)
+        .map_elements(
+            lambda x: "Indie Rock" if x in ["Rock/Indie", "Indie/Rock",
+                                            "General Indie Rock"] else x)
+        .map_elements(
+            lambda x: "Miscellaneous" if x == "misc" else x)
+        .map_elements(
+            lambda x: "Soundtrack" if x in ["soundtrack",
+                                            "Banda sonora"] else x)
+        .map_elements(
+            lambda x: "Thrash Metal" if x == "Thrash Metal" else x)
+        .map_elements(
+            lambda x: "Alt Rock" if x in ["Alt. Rock", "Alternative Rock",
+                                          "Rock alternativo",
+                                          "Alternative, Rock",
+                                          "General Alternative Rock"] else x)
+        .map_elements(
+            lambda x: "Brit Pop" if x == "Brit-pop" else x)
+        .map_elements(
+            lambda x: "Pop Rock" if x in ["Pop/Rock",
+                                          "Pop/Rock 2000's"] else x)
+        .map_elements(
+            lambda x: "Pop" if x == "General Pop" else x)
+        .map_elements(
+            lambda x: "Folk" if x == "General Folk" else x)
+        .map_elements(
+            lambda x: "Rock" if x in ["General Rock", "Rock En General",
+                                      "Rock en general", "Rock @",
+                                      "rock"] else x)
+        .map_elements(
+            lambda x: "Heavy Metal" if x == "Rock Duro Y Heavy" else x)
+        .map_elements(
+            lambda x: "Hip Hop/Rap" if x == "General Rap/Hip-Hop" else x)
+        .map_elements(
+            lambda x: "Bitpop" if x == "bitpop" else x)
+        .map_elements(
+            lambda x: "Chillstep" if x == "chillstep" else x)
+        .map_elements(
+            lambda x: "Chiptune" if x == "chiptune" else x)
+        .map_elements(
+            lambda x: None
+            if x in ["genre", "default", ".", "(255)", "Other"] else x)
+        .map_elements(
+            lambda x: "Unclassifiable" if x == "General Unclassifiable" else x)
+        .map_elements(
+            lambda x: "Soft Rock / Alternative Folk / Folk / Rock"
+            if x == "soft rock/alternative folk/folk/rock" else x)
+        .alias("Genre")
+    ).to_series()
+
+    songs_df = songs_df.with_columns(genres.alias("Genre"))
+    songs_df = songs_df.drop_nulls(["Genre"])
+    songs_df = songs_df.drop_nulls(["Year"])
+    songs_df = songs_df.filter(pl.col('Year') > 1000).filter(
+        pl.col('Year') < 2024)
+    songs_df.drop_in_place("Comments")
+    songs_df.drop_in_place("File Folder Count")
+    songs_df.drop_in_place("Library Folder Count")
+    songs_df.drop_in_place("Kind")
+    songs_df.drop_in_place("Location")
+
+    return songs_df
