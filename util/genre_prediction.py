41ba45e7874839a503577226e536a155bf86bfcf
Underdoge <eduardo.chapa@gmail.com>
2023-11-12 05:14:04 -0600

    test

--- util/genre_prediction.py
+++ util/genre_prediction.py
@@ -2,6 +2,7 @@
 functions for our streamlit app.
 """
 import librosa
+import tensorflow as tf
 import numpy as np
 from numpy import mean, var, array
 import streamlit as st
@@ -56,7 +57,7 @@ def feature_extraction(filename: str) -> array:
     return np.array(all_features)
 
 
-def pick_random_song(model,
+def pick_random_song(model: tf.keras.Sequential,
                      standard_scaler: StandardScaler, labels: list) -> None:
     """ This function will pick a random sample song and use the 'model' to
     predict its genre, extract the song's musical features using the
